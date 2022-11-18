aa10959ef38f86c5ad508a080e2d03124002c968
Paul Taylor <paul@whammydiver.com>
2022-11-18 13:50:25 -0500

    route pseudos

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,5 @@
 /*
-* All routes for Users are defined here
-* Since this file is loaded in server.js into api/users,
-*   these routes are mounted onto /users
-* See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+consider all of this pseudo for now
 */
 
 const express = require('express');
