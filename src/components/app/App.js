fc41b5d39e1a7c6ef1c64eaa6b7b8579bd83cf53
Marilynn Stone <hello@marilynnstone.com>
2022-11-20 10:34:32 -0800

    Set user token and display login if token is falsy.

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,20 @@
-import "./App.css";
+import { useState } from "react";
+// import ReactDOM from "react-dom";
+import "./App.scss";
 import Header from "../../Header";
-import Dashboard from "../user_dashboard/Dashboard";
-import DietarySettings from "../user_dashboard/DietarySettings";
-import AccountInfo from "../user_dashboard/AccountInfo";
+import Login from "../Login/Login";
+// import Dashboard from "../user_dashboard/Dashboard";
+// import DietarySettings from "../user_dashboard/DietarySettings";
+// import AccountInfo from "../user_dashboard/AccountInfo";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
+  const [token, setToken] = useState();
+
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
+
   return (
     <>
       <Header />
@@ -19,6 +29,19 @@ function App() {
           <h1>Home Page "/"</h1>
         </div>
       </div>
+      {/* <div className="wrapper">
+        <h1>Application</h1>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route path="/DietarySettings">
+              <DietarySettings />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </div> */}
     </>
   );
 }
