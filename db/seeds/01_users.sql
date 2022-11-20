2e24b1c24896f9e96270c9a58e3d7980c90e3a16
Paul Taylor <paul@whammydiver.com>
2022-11-20 14:53:34 -0500

    update db seeds with team creds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -2,5 +2,5 @@ INSERT INTO users (first_name, last_name, email, password, cellphone_number) VAL
 ('Guest_user', 'Guest_user', 'null', 'null', 'null', FALSE, FALSE),
 ('Courtney', 'O''Donnell', 'courtney@gmail.com', '$2a$10$/yOu5BpnlW86K8YP1GpII.RAnuzNh8Uzy5KxnI1BeY2mZlN5uyvO6', '+15062624316'),
 ('Paul', 'Taylor', 'paul@gmail.com', '$2a$10$/yOu5BpnlW86K8YP1GpII.RAnuzNh8Uzy5KxnI1BeY2mZlN5uyvO6', '+19053341165'),
-('Courtney', 'O''Donnell', 'courtneyadmin@gmail.com', '$2a$10$/yOu5BpnlW86K8YP1GpII.RAnuzNh8Uzy5KxnI1BeY2mZlN5uyvO6', '+19053341165'),
-('Paul', 'Taylor', 'pauladmin@gmail.com', '$2a$10$/yOu5BpnlW86K8YP1GpII.RAnuzNh8Uzy5KxnI1BeY2mZlN5uyvO6', '+19053341165'),
+('Marilynn', 'Stone', 'marilynn@gmail.com', '$2a$10$/yOu5BpnlW86K8YP1GpII.RAnuzNh8Uzy5KxnI1BeY2mZlN5uyvO6', '+19053341165'),
+('Trevor', 'Funk', 'trevor@gmail.com', '$2a$10$/yOu5BpnlW86K8YP1GpII.RAnuzNh8Uzy5KxnI1BeY2mZlN5uyvO6', '+19053341165'),
